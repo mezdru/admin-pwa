f339a66ae02e84a02d8c5e6a576d03267ff77dba
mezdru <quentin.drumez@isen.yncrea.fr>
2020-02-10 14:44:10 +0100

    refacto(organisation): Handle new organisation model

--- src/routes/RoutesWithOrgTag.js
+++ src/routes/RoutesWithOrgTag.js
@@ -1,53 +1,81 @@
-import React from 'react';
-import {Switch} from 'react-router-dom';
-import RouteWithSubRoutes from './RouteWithSubRoutes';
-import {inject, observer} from 'mobx-react';
-import {Redirect} from 'react-router-dom';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import UrlService from '../services/url.service';
+import React from "react";
+import { Switch } from "react-router-dom";
+import RouteWithSubRoutes from "./RouteWithSubRoutes";
+import { inject, observer } from "mobx-react";
+import { Redirect } from "react-router-dom";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import UrlService from "../services/url.service";
 
 class RoutesWithOrgTag extends React.Component {
-
   state = {
     render: false,
     redirect404: false
-  }
+  };
 
   componentDidMount() {
-    this.props.orgStore.getOrFetchOrganisation(null, this.props.match.params.orgTag)
-    .then((org) => {
-      if(org && org.featuredWingsFamily && org.featuredWingsFamily.length > 0 && ! org.featuredWingsFamily[0]._id) {
-        this.props.orgStore.fetchOrganisation(org._id)
-        .then(() => {
-          this.setState({render: true})
-        }).catch(e => {
-          this.setState({render: true})
-        })
-      } else {
-        this.setState({render: true})
-      }
-    })
-    .catch((e) => {
-      this.setState({redirect404: true});
-    })
+    this.props.orgStore
+      .getOrFetchOrganisation(null, this.props.match.params.orgTag)
+      .then(org => {
+        if (
+          (undefsafe(org, "settings.wings.families.length") > 0 &&
+            !org.settings.wings.families[0]._id) ||
+          (undefsafe(org, "settings.search.tabs.length") > 0 && !org.settings.search.tabs[0]._id)
+        ) {
+          this.props.orgStore
+            .fetchOrganisation(org._id)
+            .then(() => {
+              this.setState({ render: true });
+            })
+            .catch(e => {
+              this.setState({ render: true });
+            });
+        } else {
+          this.setState({ render: true });
+        }
+      })
+      .catch(e => {
+        this.setState({ redirect404: true });
+      });
     this.props.commonStore.setUrlParams(this.props.match);
   }
-  isAdminInOrg = () => 
-  (this.props.userStore.currentUser && 
-    (this.props.userStore.currentUser.superadmin || 
-      (this.props.userStore.currentOrgAndRecord && this.props.userStore.currentOrgAndRecord.admin)
-    )
-  );
-  
-  render() {
-    const {routes} = this.props;
-    const {locale} = this.props.commonStore;
-    const { redirect404, render} = this.state;
-    if(!this.props.authStore.isAuth()) return window.location.href = UrlService.getFrontflipUrl('/signin', this.props.match.params.orgTag, this.props.commonStore.locale);
-    if(redirect404) return <Redirect to={'/' + locale + '/error/404/organisation'} push/>
-    if (!render) return <CircularProgress color="secondary" style={{position: 'fixed', top: '45%', left:0, right:0, margin: 'auto'}} />;
+  isAdminInOrg = () =>
+    this.props.userStore.currentUser &&
+    (this.props.userStore.currentUser.superadmin ||
+      (this.props.userStore.currentOrgAndRecord &&
+        this.props.userStore.currentOrgAndRecord.admin));
 
-    if(!this.isAdminInOrg()) return window.location.href = UrlService.getFrontflipUrl('', this.props.match.params.orgTag, this.props.commonStore.locale);
+  render() {
+    const { routes } = this.props;
+    const { locale } = this.props.commonStore;
+    const { redirect404, render } = this.state;
+    if (!this.props.authStore.isAuth())
+      return (window.location.href = UrlService.getFrontflipUrl(
+        "/signin",
+        this.props.match.params.orgTag,
+        this.props.commonStore.locale
+      ));
+    if (redirect404)
+      return <Redirect to={"/" + locale + "/error/404/organisation"} push />;
+    if (!render)
+      return (
+        <CircularProgress
+          color="secondary"
+          style={{
+            position: "fixed",
+            top: "45%",
+            left: 0,
+            right: 0,
+            margin: "auto"
+          }}
+        />
+      );
+
+    if (!this.isAdminInOrg())
+      return (window.location.href = UrlService.getFrontflipUrl(
+        "",
+        this.props.match.params.orgTag,
+        this.props.commonStore.locale
+      ));
 
     return (
       <Switch>
@@ -55,12 +83,13 @@ class RoutesWithOrgTag extends React.Component {
           <RouteWithSubRoutes key={i} {...route} />
         ))}
       </Switch>
-    )
+    );
   }
 }
 
-export default (inject('commonStore', 'orgStore', 'authStore', 'userStore')(
-  (observer(
-    RoutesWithOrgTag
-  )))
-);
+export default inject(
+  "commonStore",
+  "orgStore",
+  "authStore",
+  "userStore"
+)(observer(RoutesWithOrgTag));
